ed7d80700ce2b7c61807621a5005b0ee8b59a9e4
sumanp <sumanpuri55@gmail.com>
2023-03-16 15:44:49 +0545

    Add test for list wallet by currency

--- test/payment_server_web/schemas/queries/wallet_test.exs
+++ test/payment_server_web/schemas/queries/wallet_test.exs
@@ -145,4 +145,59 @@ defmodule PaymentServerWeb.Schemas.Queries.WalletTest do
       assert List.last(data["wallets"])["id"] === to_string(wallet_2.id)
     end
   end
+
+  @currency_wallet_doc """
+  query walletByCurrency($currency: String!, $user_id: ID!) {
+    walletByCurrency(currency: $currency, user_id: $user_id) {
+      id
+      currency
+      amount
+      user_id
+    }
+  }
+  """
+  describe "@wallet" do
+    test "fetch wallet by currency" do
+      assert {:ok, user} =
+               Accounts.create_user(%{
+                 name: "name 1",
+                 email: "name1@email.com"
+               })
+
+      assert {:ok, wallet_1} =
+               Accounts.create_wallet(%{
+                 currency: "USD",
+                 address: "name1@email.com",
+                 amount: "10",
+                 user_id: user.id
+               })
+
+      assert {:ok, wallet_2} =
+               Accounts.create_wallet(%{
+                 currency: "GBP",
+                 address: "name2@email.com",
+                 amount: "10",
+                 user_id: user.id
+               })
+
+      assert {:ok, wallet_2} =
+               Accounts.create_wallet(%{
+                 currency: "EUR",
+                 address: "name3@email.com",
+                 amount: "10",
+                 user_id: user.id
+               })
+
+      assert {:ok, %{data: data}} =
+               Absinthe.run(@currency_wallet_doc, Schema,
+                 variables: %{
+                   "currency" => "EUR",
+                   "user_id" => user.id
+                 }
+               )
+
+      assert data["walletByCurrency"]["id"] === to_string(wallet_2.id)
+      assert data["walletByCurrency"]["currency"] === to_string(wallet_2.currency)
+    end
+  end
 end
