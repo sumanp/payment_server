357ecc685ca6ba519b2757f255841796b30b7234
sumanp <sumanpuri55@gmail.com>
2023-03-21 16:13:05 +0545

    minor refactor

--- lib/payment_server_web/resolvers/user.ex
+++ lib/payment_server_web/resolvers/user.ex
@@ -17,26 +17,30 @@ defmodule PaymentServerWeb.Resolvers.User do
           _params,
         _
       ) do
-    to_wallet = Accounts.find_wallet_by_currency(%{user_id: to_user_id, currency: currency})
-    from_wallet = Accounts.find_wallet_by_currency(%{user_id: from_user_id, currency: currency})
+    with to_wallet <-
+           Accounts.find_wallet_by_currency(%{user_id: to_user_id, currency: currency}),
+         from_wallet <-
+           Accounts.find_wallet_by_currency(%{user_id: from_user_id, currency: currency}) do
+      amount =
+        amount
+        |> Integer.parse()
+        |> elem(0)
 
-    amount =
-      amount
-      |> Integer.parse()
-      |> elem(0)
+      # stored as cents, integer data type
+      amount = Money.new(amount * 100)
 
-    # stored as cents, integer data type
-    amount = Money.new(amount * 100)
+      if(amount <= from_wallet.amount) do
+        add_amount = Money.add(to_wallet.amount, amount)
+        subtract_amount = Money.subtract(from_wallet.amount, amount)
 
-    if(amount <= from_wallet.amount) do
-      add_amount = Money.add(to_wallet.amount, amount)
-      subtract_amount = Money.subtract(from_wallet.amount, amount)
-
-      Accounts.update_wallet(to_wallet.id, %{amount: add_amount})
-      Accounts.update_wallet(from_wallet.id, %{amount: subtract_amount})
-      {:ok, %{status: "SUCCESS", message: "Payment sent successfully"}}
+        Accounts.update_wallet(to_wallet.id, %{amount: add_amount})
+        Accounts.update_wallet(from_wallet.id, %{amount: subtract_amount})
+        {:ok, %{status: "SUCCESS", message: "Payment sent successfully"}}
+      else
+        {:ok, %{status: "ERROR", message: "Insufficient funds"}}
+      end
     else
-      {:ok, %{status: "ERROR", message: "Insufficient funds"}}
+      _err -> {:ok, %{status: "ERROR", message: "Account not found"}}
     end
   end
 end
