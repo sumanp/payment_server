97b2bdb8ebbd1f2507efa303720e7731b129695c
sumanp <sumanpuri55@gmail.com>
2023-04-04 16:42:27 +0530

    use Ecto Multi

--- lib/payment_server/accounts/wallet.ex
+++ lib/payment_server/accounts/wallet.ex
@@ -3,6 +3,8 @@ defmodule PaymentServer.Accounts.Wallet do
   import Ecto.Changeset
   import Ecto.Query
 
+  alias PaymentServer.Accounts.Wallet
+
   schema "wallets" do
     field :address, :string
     field :currency, :string
@@ -23,5 +25,32 @@ defmodule PaymentServer.Accounts.Wallet do
   def filter_by_currency(%{user_id: user_id, currency: currency} = _params) do
     from w in PaymentServer.Accounts.Wallet,
       where: w.currency == ^currency and w.user_id == ^user_id
+  end
+
+  def transfer_money(from, to, amount) do
+    add_amount = Money.add(to.amount, amount)
+    subtract_amount = Money.subtract(from.amount, amount)
+
+    to_update =
+      from Wallet,
+        where: [id: ^to.id],
+        update: [set: [amount: ^add_amount]]
+
+    from_update =
+      from Wallet,
+        where: [id: ^from.id],
+        update: [set: [amount: ^subtract_amount]]
+
+    Ecto.Multi.new()
+    |> Ecto.Multi.update_all(:to, to_update, [])
+    |> Ecto.Multi.run(:check_to, fn
+      _repo, %{to: {1, _}} -> {:ok, nil}
+      _repo, %{to: {_, _}} -> {:error, {:failed_transfer, to}}
+    end)
+    |> Ecto.Multi.update_all(:from, from_update, [])
+    |> Ecto.Multi.run(:check_from, fn
+      _repo, %{from: {1, _}} -> {:ok, nil}
+      _repo, %{from: {_, _}} -> {:error, {:failed_transfer, from}}
+    end)
   end
 end
--- lib/payment_server/transaction.ex
+++ lib/payment_server/transaction.ex
@@ -1,10 +1,17 @@
 defmodule PaymentServer.Transaction do
   alias PaymentServer.Accounts
+  alias PaymentServer.Accounts.Wallet
+  alias PaymentServer.Repo
 
-  def pay(%{to_user_id: to_user_id, from_user_id: from_user_id, amount: amount, currency: currency}) do
-        with to_wallet <-
+  def pay(%{
+        to_user_id: to_user_id,
+        from_user_id: from_user_id,
+        amount: amount,
+        currency: currency
+      }) do
+    with {:ok, to_wallet} <-
            Accounts.find_wallet_by_currency(%{user_id: to_user_id, currency: currency}),
-         from_wallet <-
+         {:ok, from_wallet} <-
            Accounts.find_wallet_by_currency(%{user_id: from_user_id, currency: currency}) do
       amount =
         amount
@@ -15,17 +22,25 @@ defmodule PaymentServer.Transaction do
       amount = Money.new(amount * 100)
 
       if amount <= from_wallet.amount do
-        add_amount = Money.add(to_wallet.amount, amount)
-        subtract_amount = Money.subtract(from_wallet.amount, amount)
-
-        Accounts.update_wallet(to_wallet.id, %{amount: add_amount})
-        Accounts.update_wallet(from_wallet.id, %{amount: subtract_amount})
-        {:ok, %{status: "SUCCESS", message: "Payment sent successfully"}}
+        transfer_money(from_wallet, to_wallet, amount)
       else
         {:ok, %{status: "ERROR", message: "Insufficient funds"}}
       end
     else
       _err -> {:ok, %{status: "ERROR", message: "Account not found"}}
+    end
+  end
+
+  def transfer_money(from, to, amount) do
+    # Use Ecto.Multi
+    Wallet.transfer_money(from, to, amount)
+    |> Repo.transaction()
+    |> case do
+      {:ok, _} ->
+        {:ok, %{status: "SUCCESS", message: "Payment sent successfully"}}
+
+      {:error, _name, _value, _changes_so_far} ->
+        {:ok, %{status: "ERROR", message: "Payment Unsuccessful"}}
     end
   end
 end
